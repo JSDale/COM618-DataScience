bbb63ec20bb9d5147f647b776249b8f4bc1ef7a1
JSDale <57218102+JSDale@users.noreply.github.com>
2022-04-20 11:27:11 +0100

    added a way to replace NaN with middle of number before and after

--- application/pre_processing/remove_null_values.py
+++ application/pre_processing/remove_null_values.py
@@ -16,7 +16,17 @@ class RemoveNullValues:
             if data_frame[i].isnull().values.any():
                 mean = data_frame[i].mean()
                 data_frame[i].fillna(mean)
+        return data_frame
 
+    @staticmethod
+    def replace_not_a_number_with_median(data_frame):
+        # this only works if there are not any consecutive NaNs
+        for i in data_frame:
+            if data_frame[i].isnull().values.any():
+                a = data_frame[i-1]
+                b = data_frame[i+1]
+                median = ((a-b)/2)+b
+                data_frame[i].fillna(median)
         return data_frame
 
     @staticmethod
