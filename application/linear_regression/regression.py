55c8191bb90766d7df05e4d5f22080b641abb1a8
JSDale <57218102+JSDale@users.noreply.github.com>
2022-04-20 15:37:08 +0100

    added lin reg

--- application/linear_regression/regression.py
+++ application/linear_regression/regression.py
@@ -1,7 +1,22 @@
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
 
 class LinearRegression:
 
     @staticmethod
+    def create_scatter_graph(data_frame, save_path):
+        print('regressing...')
+        plt.scatter(data_frame['Deaths'], data_frame['Downloads'])
+        plt.title('Downloads against Deaths')
+        plt.ylabel('Downloads')
+        plt.xlabel('Deaths')
+        plt.savefig(save_path)
+
+    @staticmethod
     def apply(data_frame):
-        print('test')
+        y = data_frame['Deaths']
+        x = data_frame['Downloads']
+        x = sm.add_constant(x)
+        model = sm.OLS(y, x).fit()
+        print(model.summary())
---
+++ application/tests/linear_regression_tests.py
@@ -0,0 +1,26 @@
+import unittest
+import os
+
+from linear_regression import regression
+import import_data
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_scatter(self):
+        data = import_data.ImportData()
+        data_frame = data.read_csv(f'{os.getcwd()}\\..\\data\\deaths_against_downloads_per_month.csv')
+        reg = regression.LinearRegression()
+        save_path = f'{os.getcwd()}\\..\\graphs\\deaths_per_download.png'
+        reg.create_scatter_graph(data_frame, save_path)
+        self.assertEqual(True, True)
+
+    def test_linear_regression(self):
+        data = import_data.ImportData()
+        data_frame = data.read_csv(f'{os.getcwd()}\\..\\data\\deaths_against_downloads_per_month.csv')
+        reg = regression.LinearRegression()
+        reg.apply(data_frame)
+
+
+if __name__ == '__main__':
+    unittest.main()
