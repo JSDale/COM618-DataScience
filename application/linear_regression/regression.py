33a3437b87f37f6cd03aceae62e0c6f2be74d4e4
JSDale <57218102+JSDale@users.noreply.github.com>
2022-04-20 16:43:05 +0100

    added dropping NaNs to linear regression

--- application/linear_regression/regression.py
+++ application/linear_regression/regression.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 
+from pre_processing import remove_null_values as processor
+
 
 class LinearRegression:
 
     @staticmethod
     def create_scatter_graph(data_frame, save_path):
-        print('regressing...')
+        data_frame = processor.RemoveNullValues.drop_not_a_number(data_frame)
         plt.scatter(data_frame['Deaths'], data_frame['Downloads'])
         plt.title('Downloads against Deaths')
         plt.ylabel('Downloads')
@@ -15,6 +17,10 @@ class LinearRegression:
 
     @staticmethod
     def apply(data_frame):
+        for i in range(len(data_frame['Week starting'])):
+            print(data_frame['Week starting'][i])
+            print(data_frame['Week ending'][i])
+        data_frame = processor.RemoveNullValues.drop_not_a_number(data_frame)
         y = data_frame['Deaths']
         x = data_frame['Downloads']
         x = sm.add_constant(x)
--- application/tests/linear_regression_tests.py
+++ application/tests/linear_regression_tests.py
@@ -9,17 +9,20 @@ class MyTestCase(unittest.TestCase):
 
     def test_scatter(self):
         data = import_data.ImportData()
-        data_frame = data.read_csv(f'{os.getcwd()}\\..\\data\\deaths_against_downloads_per_month.csv')
+        # data_frame = data.read_csv(f'{os.getcwd()}\\..\\data\\deaths_against_downloads_per_month.csv')
+        data_frame = data.read_csv(f'{os.getcwd()}\\..\\data\\weekly_deaths_by_downloads.csv')
         reg = regression.LinearRegression()
-        save_path = f'{os.getcwd()}\\..\\graphs\\deaths_per_download.png'
+        save_path = f'{os.getcwd()}\\..\\graphs\\deaths_per_download_weekly.png'
         reg.create_scatter_graph(data_frame, save_path)
         self.assertEqual(True, True)
 
     def test_linear_regression(self):
         data = import_data.ImportData()
-        data_frame = data.read_csv(f'{os.getcwd()}\\..\\data\\deaths_against_downloads_per_month.csv')
+        # data_frame = data.read_csv(f'{os.getcwd()}\\..\\data\\deaths_against_downloads_per_month.csv')
+        data_frame = data.read_csv(f'{os.getcwd()}\\..\\data\\weekly_deaths_by_downloads.csv')
         reg = regression.LinearRegression()
         reg.apply(data_frame)
+        self.assertEqual(True, True)
 
 
 if __name__ == '__main__':
