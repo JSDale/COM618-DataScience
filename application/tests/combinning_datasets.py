346e8c05d319eda9217dc00c108fb9b2be882a9d
JSDale <57218102+JSDale@users.noreply.github.com>
2022-04-20 16:14:33 +0100

    added better display

--- application/tests/combinning_datasets.py
+++ application/tests/combinning_datasets.py
@@ -7,13 +7,15 @@ from pre_processing import combinning_data
 
 class MyTestCase(unittest.TestCase):
 
-    def test_something(self):
+    def test_combining(self):
         data = ImportData()
         data_frame_deaths = data.read_csv(f'{os.getcwd()}\\..\\data\\_nhse_total_deaths_by_region.csv')
         data_frame_downloads = data.read_csv(f'{os.getcwd()}\\..\\data\\covid19_app_country_agnostic_dataset.csv')
         com = combinning_data.Combine()
         result = com.deaths_and_downloads(data_frame_deaths, data_frame_downloads)
-        print(result)
+        for i in range(len(result)):
+            print(result[i])
+        print(len(result))
         self.assertEqual(True, True)  # add assertion here
 
 
