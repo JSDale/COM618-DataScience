1d224c60d79dc43583e8d351cbb6d93bc0731501
JSDale <57218102+JSDale@users.noreply.github.com>
2022-04-25 17:15:54 +0100

    added more

--- application/tests/linear_regression_tests.py
+++ application/tests/linear_regression_tests.py
@@ -39,6 +39,36 @@ class MyTestCase(unittest.TestCase):
         reg.apply(data_frame, 'new_deaths', 'new_downloads')
         self.assertEqual(True, True)
 
+    def test_scatter_monthly_new_no_outlier(self):
+        data = self.data
+        data_frame = data.read_csv(f'{os.getcwd()}\\..\\data\\deaths_against_downloads_per_month_no_outlier.csv')
+        reg = regression.LinearRegression()
+        save_path = f'{os.getcwd()}\\..\\graphs\\deaths_per_download_monthly_new_no_outlier.png'
+        reg.create_scatter_graph(data_frame, save_path, 'new_deaths', 'new_downloads')
+        self.assertEqual(True, True)
+
+    def test_scatter_weekly_new_no_outlier(self):
+        data = self.data
+        data_frame = data.read_csv(f'{os.getcwd()}\\..\\data\\weekly_deaths_by_downloads_no_outlier.csv')
+        reg = regression.LinearRegression()
+        save_path = f'{os.getcwd()}\\..\\graphs\\deaths_per_download_weekly_no_outlier.png'
+        reg.create_scatter_graph(data_frame, save_path, 'new_deaths', 'new_downloads')
+        self.assertEqual(True, True)
+
+    def test_linear_regression_monthly_new_no_outlier(self):
+        data = self.data
+        data_frame = data.read_csv(f'{os.getcwd()}\\..\\data\\deaths_against_downloads_per_month_no_outlier.csv')
+        reg = regression.LinearRegression()
+        reg.apply(data_frame, 'new_deaths', 'new_downloads')
+        self.assertEqual(True, True)
+
+    def test_linear_regression_weekly_new_no_outlier(self):
+        data = self.data
+        data_frame = data.read_csv(f'{os.getcwd()}\\..\\data\\weekly_deaths_by_downloads_no_outlier.csv')
+        reg = regression.LinearRegression()
+        reg.apply(data_frame, 'new_deaths', 'new_downloads')
+        self.assertEqual(True, True)
+
     def test_scatter_monthly_cumulative(self):
         data = self.data
         data_frame = data.read_csv(f'{os.getcwd()}\\..\\data\\deaths_against_downloads_per_month.csv')
